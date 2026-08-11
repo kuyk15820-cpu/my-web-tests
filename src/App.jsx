10fb0d7565dafceddc8571601c62ead554e014dc
kuyk15820-cpu <kuyk15820@gmail.com>
2026-08-11 19:30:59 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,22 +21,8 @@ if (false) {
 }
 
 const PACKAGES = [
-  { id: 'batterymirror', name: 'CookieRun Classic', version: '26.7.11', bundle: 'com.devsisters.crg', price: 'Free', icon: 'CookieRunClassic.png' },
-  { id: 'battfx', name: 'BattFX', version: 'v0.0.5', bundle: 'com.futur3sn0w.battfx', price: 'Free', icon: 'com.futur3sn0w.battfx.png' },
-  { id: 'ccaster', name: 'CCAster', version: 'v0.2.2', bundle: 'com.futur3sn0w.ccaster', price: 'Free', icon: 'com.futur3sn0w.ccaster.png' },
-  { id: 'ccsupportbatteryfix', name: 'CCSupport Battery Fix', version: 'v0.0.2', bundle: 'com.futur3sn0w.ccsupportbatteryfix', price: 'Free', icon: 'com.futur3sn0w.ccsupportbatteryfix.png' },
-  { id: 'centerlastrow', name: 'CenterLastRow', version: 'v0.0.1-2+debug', bundle: 'com.futur3sn0w.centerlastrow', price: 'Free', icon: 'com.futur3sn0w.centerlastrow.png' },
-  { id: 'custhome', name: 'CustHome', version: 'v0.2.0', bundle: 'com.futur3sn0w.custhome', price: 'Free', icon: 'com.futur3sn0w.custhome.png' },
-  { id: 'dockfull', name: 'DockFull', version: 'v0.0.3', bundle: 'com.futur3sn0w.dockfull', price: 'Free', icon: 'com.futur3sn0w.dockfull.png' },
-  { id: 'docklibrary', name: 'DockLibrary', version: 'v0.4.2', bundle: 'com.futur3sn0w.docklibrary', price: 'Free', icon: 'com.futur3sn0w.docklibrary.png' },
-  { id: 'duowall', name: 'DuoWall', version: 'v0.0.76-2+debug', bundle: 'com.futur3sn0w.duowall', price: 'Free', icon: 'com.futur3sn0w.duowall.png' },
-  { id: 'finn', name: 'Finn', version: 'v1.0.1', bundle: 'com.futur3sn0w.finn', price: 'Free', icon: 'com.futur3sn0w.finn.png' },
-  { id: 'muteflash', name: 'MuteFlash', version: 'v0.0.2', bundle: 'com.futur3sn0w.muteflash', price: 'Free', icon: 'com.futur3sn0w.muteflash.png' },
-  { id: 'mutemodule', name: 'MuteModule', version: 'v0.0.1-4+debug', bundle: 'com.futur3sn0w.mutemodule', price: 'Free', icon: 'com.futur3sn0w.mutemodule.png' },
-  { id: 'noseparators', name: 'NoSeparators', version: 'v0.0.1-2+debug', bundle: 'com.futur3sn0w.noseparators', price: 'Free', icon: 'com.futur3sn0w.noseparators.png' },
-  { id: 'solert', name: 'Solert', version: 'v0.0.1-1+debug', bundle: 'com.futur3sn0w.solert', price: 'Free', icon: 'com.futur3sn0w.solert.png' },
-  { id: 'swipeformore7', name: 'SwipeForMore7', version: 'v1.2.5+ios7.12', bundle: 'com.futur3sn0w.swipeformore7', price: 'Free', icon: 'com.futur3sn0w.swipeformore7.png' },
-  { id: 'taptimenneo', name: 'TapTimeNeo', version: 'v0.0.1-7+debug', bundle: 'com.futur3sn0w.taptimenneo', price: 'Free', icon: 'com.futur3sn0w.taptimenneo.png' },
+  { id: 'cookierunclassic', name: 'CookieRun Classic', version: '26.7.11', bundle: 'com.devsisters.crg', price: 'Free', server: 'GL', type: 'Decrypt', icon: 'CookieRunClassic.png' },
+  { id: 'garenarov', name: 'Garena Rov', version: '1.63.1.10', bundle: 'com.garena.game.kgth', price: 'Free', server: 'TH', type: 'Decrypt', icon: 'GarenaRov.png' }
 ];
 
 export default function App() {
@@ -72,7 +58,11 @@ export default function App() {
               <div className="pkg-name">{pkg.name}</div>
               <div className="pkg-version">{pkg.version}</div>
               <div className="pkg-bundle">{pkg.bundle}</div>
-              <div className="pkg-price">{pkg.price}</div>
+              <div className="pkg-tags">
+                {pkg.price && <span className="pkg-price">{pkg.price}</span>}
+                {pkg.server && <span className="pkg-server">{pkg.server}</span>}
+                {pkg.type && <span className="pkg-type">{pkg.type}</span>}
+              </div>
             </div>
           </div>
         ))}
