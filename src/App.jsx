38276a356511e37a5f7a622e4ee8e9af62c7ca1f
kuyk15820-cpu <kuyk15820@gmail.com>
2026-08-12 02:40:00 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,37 @@ if (false) {
 }
 
 const PACKAGES = [
-  { id: 'cookierunclassic', name: 'CookieRun Classic', server: 'GL', version: '26.7.11', bundle: 'com.devsisters.crg', price: 'Free', type: 'Decrypt', icon: 'CookieRunClassic.png' },
-  { id: 'rov', name: 'Garena Rov', server: 'TH', version: '1.63.11716331', bundle: 'com.garena.game.kgth', price: 'Free', type: 'Decrypt', icon: 'Rov.png' },
-  { id: 'mlbb', name: 'Mobile Legends: Bang Bang', server: 'GL', version: '2.1.95', bundle: 'com.mobile.legends', price: 'Free', type: 'Decrypt', icon: 'Mlbb.png' }
+  { 
+    id: 'rov', 
+    name: 'Garena RoV x DAN DA DAN', 
+    server: 'TH', 
+    version: '1.63.11716331', 
+    bundle: 'com.garena.game.kgth', 
+    price: 'Free', 
+    type: 'Decrypt', 
+    icon: 'Rov.png',
+    downloadUrl: '/apps/com.garena.game.kgth_1.63.11716331_F1X3R-Decrypt.ipa'
+  },
+  { 
+    id: 'mlbb', 
+    name: 'Mobile Legends: Bang Bang', 
+    server: 'GL', 
+    version: '2.1.95', 
+    bundle: 'com.mobile.legends', 
+    price: 'Free', 
+    type: 'Decrypt', 
+    icon: 'Mlbb.png' 
+  },
+  { 
+    id: 'cookierunclassic', 
+    name: 'CookieRun Classic', 
+    server: 'GL', 
+    version: '26.7.11', 
+    bundle: 'com.devsisters.crg', 
+    price: 'Free', 
+    type: 'Decrypt', 
+    icon: 'CookieRunClassic.png' 
+  }
 ];
 
 export default function App() {
@@ -59,32 +87,50 @@ export default function App() {
       </div>
 
       <div className="pkg-grid">
-        {PACKAGES.map((pkg) => (
-          <div className="pkg-card" key={pkg.id}>
-            <img 
-              className="pkg-icon" 
-              src={`/images/${pkg.icon}`} 
-              alt={`${pkg.name} icon`} 
-            />
-            <div className="pkg-info">
-              <div className="pkg-name">
-                {pkg.name}
-                {pkg.server && (
-                  <>
-                    &nbsp;&middot;&nbsp;
-                    <span className="pkg-server-text">{pkg.server}</span>
-                  </>
-                )}
+        {PACKAGES.map((pkg) => {
+          const CardContent = (
+            <>
+              <img 
+                className="pkg-icon" 
+                src={`/images/${pkg.icon}`} 
+                alt={`${pkg.name} icon`} 
+              />
+              <div className="pkg-info">
+                <div className="pkg-name">
+                  {pkg.name}
+                  {pkg.server && (
+                    <>
+                      &nbsp;&middot;&nbsp;
+                      <span className="pkg-server-text">{pkg.server}</span>
+                    </>
+                  )}
+                </div>
+                <div className="pkg-version">Version: {pkg.version}</div>
+                <div className="pkg-bundle">Identifier: {pkg.bundle}</div>
+                <div className="pkg-tags">
+                  {pkg.price && <span className="pkg-price">{pkg.price}</span>}
+                  {pkg.type && <span className="pkg-type">{pkg.type}</span>}
+                </div>
               </div>
-              <div className="pkg-version">Version: {pkg.version}</div>
-              <div className="pkg-bundle">Identifier: {pkg.bundle}</div>
-              <div className="pkg-tags">
-                {pkg.price && <span className="pkg-price">{pkg.price}</span>}
-                {pkg.type && <span className="pkg-type">{pkg.type}</span>}
-              </div>
+            </>
+          );
+
+          return pkg.downloadUrl ? (
+            <a 
+              key={pkg.id} 
+              href={pkg.downloadUrl} 
+              download 
+              className="pkg-card"
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              {CardContent}
+            </a>
+          ) : (
+            <div className="pkg-card" key={pkg.id}>
+              {CardContent}
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <div className="divider"></div>
